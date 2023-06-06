1eee082d45fa86436573a95642a770deaef80d04
Exlamux6 <lx04717@innovaccion.mx>
2023-06-06 07:28:36 -0600

    mejoras de la vista de las diferentes interfaces

--- cuartogrado.php
+++ cuartogrado.php
@@ -18,23 +18,37 @@
 <body style="background-color:rgb(224, 224, 212)">
     <!--Inicia el header-->
     <header>
-        <div class="container-fluid">
-            <footer class="d-flex container flex-wrap justify-content-between align-items-center py-1 my-1 border-top">
-                <div class="col-md-4 d-flex align-items-center">
-                    <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                        <img src="img/tic.png" alt="" width="150px">
-                    </a>
+        <!-- Topbar Start -->
+        <div class="container-fluid text-light p-0" id="TopbarStar">
+            <div class="container ">
+                <div class="row gx-0 d-none d-lg-flex">
+                    <div class="col-lg-3 text-start">
+                        <div class="h-100 d-inline-flex align-items-center me-4">
+                            <img src="img/cct.png" alt="" style="margin-right: 10px;" width="18px">
+                            <small>CCT: 12DPR2019N</small>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 text-end">
+                        <div class="h-100 d-inline-flex align-items-center me-4">
+                                <small>
+                                    <script>
+                                        var meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+                                        var f = new Date();
+                                        document.write(f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear());
+                                    </script>
+                                </small>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 px-5 text-end">
+                        <div class="h-100 d-inline-flex align-items-center">
+                            <img src="img/reloj.png" alt="" style="margin-right: 10px;" width="18px">
+                            <small>LUNES - VIERNES : 08:00 AM - 12:30 PM</small>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="nav col-md-4 justify-content-end list-unstyled d-flex">
-                    <script>
-                        var meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-                        var f = new Date();
-                        document.write(f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear());
-                    </script>
-                </div>
-            </footer>
+            </div>
         </div>
+        <!-- Topbar End -->
     </header>
     <!--Aquí termina el header-->
 
